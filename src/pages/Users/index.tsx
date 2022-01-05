64f5416302a349955a3ce2753cc42ca76f3995b5
MatheusOliveira <mateusribeirooliveira@hotmail.com>
2022-01-05 17:11:24 -0300

    chore: setup axios configuration

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
 import AppTable from 'src/components/shared/app-table'
 
+import { useAppDispatch } from "src/app/hooks";
+import { setUsers } from "src/app/store/slices/users";
+import { getUsers } from 'src/service/api/users'
+
 export default function User() {
-   
+    const dipatch = useAppDispatch()
+    useEffect(() => {
+        getUsers().then((response) => {
+            dipatch(setUsers(response))
+        })
+    }, [])
+
     return (
         <div>
-            <AppTable/>
+            <AppTable />
         </div>
     )
 }
--- src/service/api/users/index.ts
+++ src/service/api/users/index.ts
@@ -1,5 +1,8 @@
 import http from "src/settings/http";
+import { Users } from "src/app/definitions";
 
-export async function getUsers () {
-    
+export async function getUsers(): Promise<Users> {
+    const response = await http.get('/users')
+    const data = response.data
+    return data
 }
