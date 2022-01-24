5539c281246f311b5b56c370a66e17dc9b780a62
MatheusOliveira <mateusribeirooliveira@hotmail.com>
2022-01-24 12:21:58 -0300

    chore: Installed and configured dependency redux persist

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,13 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers'
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+    })
+  }
 })
 
+
+const persistor = persistStore(store)
+
+const defaultStore = {
+  store, persistor
+}
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
 
-export default store
+export default defaultStore
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from 'src/App';
 import 'src/i18n';
-import store from 'src/app/store'
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import defaultStoreConfig from './app/store';
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={defaultStoreConfig.store}>
+      <PersistGate loading={null} persistor={defaultStoreConfig.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
